5ab8e0ca7a66cbeb9a5f665ccc6425d8c504d7aa
Rohit13SQFT <rohit.kumar@13sqft.com>
2025-04-29 15:38:28 +0530

    code optimized

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { ConsumerService } from './services/consumer.service';
+import { ProducerService } from './services/producer.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(private producerService: ProducerService, private consumerService: ConsumerService) {
+  }
+
+  async getHello() {
+    await this.producerService.produce({
+      topic: 'test',
+      messages: [{
+        value: "hello kafka"
+      }]
+    })
+
+    this.consumerService.consume(
+      { topic: 'test' },
+      {
+        eachMessage: async ({ topic, partition, message }) => {
+          console.log({
+            value: message.value?.toString(),
+            topic: topic?.toString(),
+            partition: partition?.toString()
+          });
+        }
+      }
+    )
     return 'Hello World!';
   }
+
 }
